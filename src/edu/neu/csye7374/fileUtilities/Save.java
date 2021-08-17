c208c91351d11f01d6f6a578c79ba5e6a3f84615
Yin Song <song.yin1@husky.neu.edu>
2021-08-17 14:39:38 -0400

    Update Save.java
    saveAll() to a company

--- src/edu/neu/csye7374/fileUtilities/Save.java
+++ src/edu/neu/csye7374/fileUtilities/Save.java
@@ -2,6 +2,7 @@ package edu.neu.csye7374.fileUtilities;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,6 +10,8 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.neu.csye7374.model.Company;
+import edu.neu.csye7374.model.Item;
 import edu.neu.csye7374.model.Order;
 import edu.neu.csye7374.model.Person;
 
@@ -16,9 +19,17 @@ public class Save {
 
 	private String person_file = "persons.ser";
 	private String order_file = "orders.ser";
+	private String items_file = "items.ser";
+	private String company_file = "company.ser";
+	private Company company;
 
 	public Save() {
 
+	}
+
+	public Save(Company company) {
+		// TODO Auto-generated constructor stub
+		this.company = company;
 	}
 
 	public void savePersons(List<Person> persons) throws IOException {
@@ -45,7 +56,28 @@ public class Save {
 		f.close();
 	}
 
-	public void saveAll() {
+	public void saveItems(List<Item> items) throws IOException {
+
+		FileOutputStream f = new FileOutputStream(new File(items_file));
+		ObjectOutputStream o = new ObjectOutputStream(f);
+
+		// Write objects to file
+		o.writeObject(items);
+
+		o.close();
+		f.close();
+	}
+
+	public void saveAll() throws IOException {
+
+		FileOutputStream f = new FileOutputStream(new File(company_file));
+		ObjectOutputStream o = new ObjectOutputStream(f);
+
+		// Write objects to file
+		o.writeObject(company);
+
+		o.close();
+		f.close();
 
 	}
 
