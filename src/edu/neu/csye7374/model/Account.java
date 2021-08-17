1bb122d0c848e79f08f1f18d1803751da8b969ae
Yin Song <song.yin1@husky.neu.edu>
2021-08-17 12:55:16 -0400

    Update Account.java
    add status string

--- src/edu/neu/csye7374/model/Account.java
+++ src/edu/neu/csye7374/model/Account.java
@@ -5,14 +5,38 @@ import java.io.Serializable;
 public class Account implements Serializable{
 	
 	private String userName;
-	
 	private String password;
+	private String status;
 	
 	
 	public Account() {
 		this.userName = "user";
 		this.password = "password";
+	
 	}
+	
+	
+
+	public Account(String userName, String password, String status) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.status = status;
+	}
+
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
 
 	public Account(String userName, String password) {
 		super();
