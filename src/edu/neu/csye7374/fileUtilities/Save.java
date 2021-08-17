5446affb3b061c1bde45da73338d2a3b05ef0109
Yin Song <song.yin1@husky.neu.edu>
2021-08-17 19:51:22 -0400

    Update Save.java

--- src/edu/neu/csye7374/fileUtilities/Save.java
+++ src/edu/neu/csye7374/fileUtilities/Save.java
@@ -1,34 +1,23 @@
 package edu.neu.csye7374.fileUtilities;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.io.FileOutputStream;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import edu.neu.csye7374.model.Company;
-import edu.neu.csye7374.model.Item;
-import edu.neu.csye7374.model.Order;
-import edu.neu.csye7374.model.Person;
 
 public class Save {
 
 	private String company_file = "company.ser";
 	private Company company;
 
-	public Save() {
-
-	}
-
 	public Save(Company company) {
 		// TODO Auto-generated constructor stub
 		this.company = company;
 	}
-
 
 	public void saveAll() throws IOException {
 
@@ -36,7 +25,7 @@ public class Save {
 		ObjectOutputStream o = new ObjectOutputStream(f);
 
 		// Write objects to file
-		o.writeObject(company);
+		o.writeObject(this.company);
 
 		o.close();
 		f.close();
