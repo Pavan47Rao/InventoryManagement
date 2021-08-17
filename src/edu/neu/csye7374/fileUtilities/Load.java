433713d2ae6dde1e16f1a9a9578fe5c0946ad20e
Yin Song <song.yin1@husky.neu.edu>
2021-08-17 15:54:55 -0400

    Update Load.java

--- src/edu/neu/csye7374/fileUtilities/Load.java
+++ src/edu/neu/csye7374/fileUtilities/Load.java
@@ -1,79 +1,39 @@
 package edu.neu.csye7374.fileUtilities;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.util.Scanner;
-
 import edu.neu.csye7374.model.Company;
-import edu.neu.csye7374.model.Item;
-import edu.neu.csye7374.model.Order;
+
 import edu.neu.csye7374.model.Person;
 
 public class Load {
 
 	private Company company;
-	private String person_file = "persons.ser";
-	private String order_file = "orders.ser";
-	private String items_file = "items.ser";
 	private String company_file = "company.ser";
 
 	public Load() {
-		
+
 	}
 
 	public Load(Company company) {
 		// TODO Auto-generated constructor stub
-		this.company=company;
+		this.company = company;
 	}
 
-	// Load items 
-	@SuppressWarnings("unchecked")
-	public List<Item> loadAllItems() throws IOException, ClassNotFoundException {
-		List<Item> items = new ArrayList<>();
-		FileInputStream fis = new FileInputStream(items_file);
-		@SuppressWarnings("resource")
-		ObjectInputStream ois = new ObjectInputStream(fis);
-		
-		items = (List<Item>) ois.readObject();
-		
-		System.out.println("Start loading items...");
-		for (int i = 0; i < items.size(); i++) {
-			System.out.println(items.get(i));
-		}
-		
-		this.company.setItems(items);
-		return items;
-	}
-	
 	// Load all persons
-	@SuppressWarnings("unchecked")
 	public List<Person> loadAllPersons() throws IOException, ClassNotFoundException {
-		List<Person> persons = new ArrayList<>();
-		FileInputStream fis = new FileInputStream(person_file);
-		@SuppressWarnings("resource")
-		ObjectInputStream ois = new ObjectInputStream(fis);
+		Company tempCompany = loadAll();
 
-		persons = (List<Person>) ois.readObject();
-		System.out.println("Start loading persons...");
-		for (int i = 0; i < persons.size(); i++) {
-			System.out.println(persons.get(i));
-		}
-
-		this.company.setPeople(persons);
-		return persons;
+		return tempCompany.getPeople();
 	}
 
-	//ID=1 is Manager, ID=2 is HR, ID=3 is Supplier
-	// load all suppliers 
-	@SuppressWarnings("unchecked")
+	// ID=1 is Manager, ID=2 is HR, ID=3 is Supplier
+	// load all suppliers
 	public List<Person> loadSuppliers() throws IOException, ClassNotFoundException {
 		List<Person> persons = loadAllPersons();
 		List<Person> suppliers = new ArrayList<>();
@@ -85,9 +45,8 @@ public class Load {
 
 		return suppliers;
 	}
-	
-	//load all HRs
-	@SuppressWarnings("unchecked")
+
+	// load all HRs
 	public List<Person> loadHR() throws IOException, ClassNotFoundException {
 		List<Person> persons = loadAllPersons();
 		List<Person> hr = new ArrayList<>();
@@ -96,12 +55,11 @@ public class Load {
 				hr.add(persons.get(i));
 			}
 		}
-		
+
 		return hr;
 	}
-	
-	//load all managers 
-	@SuppressWarnings("unchecked")
+
+	// load all managers
 	public List<Person> loadManagers() throws IOException, ClassNotFoundException {
 		List<Person> persons = loadAllPersons();
 		List<Person> managers = new ArrayList<>();
@@ -110,27 +68,8 @@ public class Load {
 				managers.add(persons.get(i));
 			}
 		}
-		
+
 		return managers;
-	}
-	
-	//load all orders 
-	@SuppressWarnings("unchecked")
-	public List<Order> loadAllOrders() throws IOException, ClassNotFoundException {
-		List<Order> orders = new ArrayList<>();
-		FileInputStream fis = new FileInputStream(order_file);
-		@SuppressWarnings("resource")
-		ObjectInputStream ois = new ObjectInputStream(fis);
-
-		orders = (List<Order>) ois.readObject();
-
-		System.out.println("Start loading orders...");
-		for (int i = 0; i < orders.size(); i++) {
-			System.out.println(orders.get(i));
-		}
-
-		this.company.setOrders(orders);
-		return orders;
 	}
 
 	public Company loadAll() throws IOException, ClassNotFoundException {
