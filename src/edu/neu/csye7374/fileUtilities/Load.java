f398a87a7d00c429cb61c6e11ae68ac7aed9b383
Yin Song <song.yin1@husky.neu.edu>
2021-08-17 14:42:57 -0400

    Update Load.java

--- src/edu/neu/csye7374/fileUtilities/Load.java
+++ src/edu/neu/csye7374/fileUtilities/Load.java
@@ -12,18 +12,44 @@ import java.util.List;
 
 import java.util.Scanner;
 
+import edu.neu.csye7374.model.Company;
+import edu.neu.csye7374.model.Item;
 import edu.neu.csye7374.model.Order;
 import edu.neu.csye7374.model.Person;
 
 public class Load {
 
+	private Company company;
 	private String person_file = "persons.ser";
 	private String order_file = "orders.ser";
+	private String items_file = "items.ser";
+	private String company_file = "company.ser";
 
 	public Load() {
 		
 	}
 
+	public Load(Company company) {
+		// TODO Auto-generated constructor stub
+		this.company=company;
+	}
+
+	// Load items 
+	@SuppressWarnings("unchecked")
+	public List<Item> loadAllItems() throws IOException, ClassNotFoundException {
+		List<Item> items = new ArrayList<>();
+		FileInputStream fis = new FileInputStream(items_file);
+		@SuppressWarnings("resource")
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		items = (List<Item>) ois.readObject();
+		for (int i = 0; i < items.size(); i++) {
+			System.out.println(items.get(i));
+		}
+		return items;
+	}
+	
+	// Load all persons
 	@SuppressWarnings("unchecked")
 	public List<Person> loadAllPersons() throws IOException, ClassNotFoundException {
 		List<Person> persons = new ArrayList<>();
@@ -40,12 +66,14 @@ public class Load {
 		return persons;
 	}
 
+	//ID=1 is Manager, ID=2 is HR, ID=3 is Supplier
+	// load all suppliers 
 	@SuppressWarnings("unchecked")
 	public List<Person> loadSuppliers() throws IOException, ClassNotFoundException {
 		List<Person> persons = loadAllPersons();
 		List<Person> suppliers = new ArrayList<>();
 		for (int i = 0; i < persons.size(); i++) {
-			if (persons.get(i).getAccount().getStatus() == "Supplier") {
+			if (persons.get(i).getRoleId() == 3) {
 				suppliers.add(persons.get(i));
 			}
 		}
@@ -53,12 +81,13 @@ public class Load {
 		return suppliers;
 	}
 	
+	//load all HRs
 	@SuppressWarnings("unchecked")
 	public List<Person> loadHR() throws IOException, ClassNotFoundException {
 		List<Person> persons = loadAllPersons();
 		List<Person> hr = new ArrayList<>();
 		for (int i = 0; i < persons.size(); i++) {
-			if (persons.get(i).getAccount().getStatus() == "HR") {
+			if (persons.get(i).getRoleId() == 2) {
 				hr.add(persons.get(i));
 			}
 		}
@@ -66,6 +95,21 @@ public class Load {
 		return hr;
 	}
 	
+	//load all managers 
+	@SuppressWarnings("unchecked")
+	public List<Person> loadManagers() throws IOException, ClassNotFoundException {
+		List<Person> persons = loadAllPersons();
+		List<Person> managers = new ArrayList<>();
+		for (int i = 0; i < persons.size(); i++) {
+			if (persons.get(i).getRoleId() == 1) {
+				managers.add(persons.get(i));
+			}
+		}
+		
+		return managers;
+	}
+	
+	//load all orders 
 	@SuppressWarnings("unchecked")
 	public List<Order> loadAllOrders() throws IOException, ClassNotFoundException {
 		List<Order> orders = new ArrayList<>();
@@ -82,7 +126,13 @@ public class Load {
 		return orders;
 	}
 
-	public void loadAll() throws IOException, ClassNotFoundException {
+	public Company loadAll() throws IOException, ClassNotFoundException {
+		FileInputStream fis = new FileInputStream(company_file);
+		@SuppressWarnings("resource")
+		ObjectInputStream ois = new ObjectInputStream(fis);
+
+		this.company = (Company) ois.readObject();
+		return this.company;
 
 	}
 
