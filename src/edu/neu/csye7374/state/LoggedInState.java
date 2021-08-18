096e0ece71e1167bc76c22ea4a232936a9dd0bdf
Yin Song <song.yin1@northeastern.edu>
2021-08-18 17:48:24 -0400

    Add files via upload

---
+++ src/edu/neu/csye7374/state/LoggedInState.java
@@ -0,0 +1,25 @@
+package edu.neu.csye7374.state;
+
+public class LoggedInState implements StateAPI {
+	
+	String descr = "Logged In";
+
+	@Override
+	public String stateDescr() {
+		// TODO Auto-generated method stub
+		return descr;
+	}
+
+	@Override
+	public void login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
