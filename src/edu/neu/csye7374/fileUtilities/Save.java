ab354bd1b0085b7919013398b65615f8f60214d6
Yin Song <song.yin1@husky.neu.edu>
2021-08-18 02:15:51 -0400

    Update Save.java

--- src/edu/neu/csye7374/fileUtilities/Save.java
+++ src/edu/neu/csye7374/fileUtilities/Save.java
@@ -12,6 +12,7 @@ import edu.neu.csye7374.model.Company;
 public class Save {
 
 	private String company_file = "company.ser";
+	private String stock_repo_file = "stock_repo_file.ser";
 	private Company company;
 
 	public Save(Company company) {
@@ -30,6 +31,18 @@ public class Save {
 		o.close();
 		f.close();
 
+	}
+	public void saveStockRepo() throws IOException {
+		
+		FileOutputStream f = new FileOutputStream(new File(stock_repo_file));
+		ObjectOutputStream o = new ObjectOutputStream(f);
+		
+		// Write objects to file
+		o.writeObject(this.company.getInventory().getStockRepository());
+		
+		o.close();
+		f.close();
+		
 	}
 
 }
